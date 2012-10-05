4fdb07df1feb7fd88c3e7b38215d236a2fa21a87
John Caprez <jay@jaykay-design.com>
2012-10-05 11:47:33 +0000

    git-svn-id: http://svn.jaykay-design.com:31998/svn/WDSF/API/trunk@27 18e61fbd-f885-3640-86ae-a3b2b8efa25b

---
+++ NET/Wdsf.Api.Client/Models/CoupleExport.cs
@@ -0,0 +1,48 @@
+﻿namespace Wdsf.Api.Client.Models
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+    using Wdsf.Api.Client.Attributes;
+
+    [XmlType("couple", Namespace = "http://services.worlddancesport.org/api")]
+    [XmlRoot(Namespace = "http://services.worlddancesport.org/api")]
+    [MediaType("application/vnd.worlddancesport.couple.export")]
+    public class CoupleExport : CoupleBase
+    {
+        [XmlElement("displayName")]
+        public string Name { get; set; }
+
+        [XmlElement("manName")]
+        public string ManName { get; set; }
+
+        [XmlElement("manSurname")]
+        public string ManSurname { get; set; }
+
+        [XmlElement("manNationality")]
+        public string ManNationality { get; set; }
+
+        [XmlElement("womanName")]
+        public string WomanName { get; set; }
+
+        [XmlElement("womanSurname")]
+        public string WomanSurname { get; set; }
+
+        [XmlElement("womanNationality")]
+        public string WomanNationality { get; set; }
+
+        [XmlElement("country")]
+        public string Country { get; set; }
+
+        [XmlElement("manMin")]
+        public int ManMin { get; set; }
+
+        [XmlElement("womanMin")]
+        public int WomanMin { get; set; }
+
+        [XmlElement("division")]
+        public string Division { get; set; }
+
+        [XmlElement("agegroup")]
+        public string AgeGroup { get; set; }
+    }
+}
---
+++ NET/Wdsf.Api.Client/Models/ListOfCoupleExport.cs
@@ -0,0 +1,13 @@
+﻿namespace Wdsf.Api.Client.Models
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+    using Wdsf.Api.Client.Attributes;
+
+    [XmlType("couples", Namespace = "http://services.worlddancesport.org/api")]
+    [XmlRoot("couples", Namespace = "http://services.worlddancesport.org/api")]
+    [MediaType("application/vnd.worlddancesport.couples.export", IsCollection = true)]
+    public class ListOfCoupleExport : List<CoupleExport>
+    {
+    }
+}

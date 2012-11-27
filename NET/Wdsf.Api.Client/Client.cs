00f9b8cb13a8afda9987b07ad5abf7e0a0a61057
John Caprez <jay@jaykay-design.com>
2012-11-27 08:25:30 +0000

    updated IClient for WorldRanking Method added GetWorldRanking overload for date filter moved XML documentation to Interface added GetOfficials to IClient
    git-svn-id: http://svn.jaykay-design.com:31998/svn/WDSF/API/trunk@30 18e61fbd-f885-3640-86ae-a3b2b8efa25b

--- NET/Wdsf.Api.Client/Client.cs
+++ NET/Wdsf.Api.Client/Client.cs
@@ -39,8 +39,10 @@ namespace Wdsf.Api.Client
         private List<RestAdapter> adapters;
         private readonly object adapterLock = new object();
 
+        /// <summary>
+        /// Contains the last message returned from the API
+        /// </summary>
         public string LastApiMessage { get; private set; }
-
 
         public Client(string username, string password, WdsfEndpoint endPoint) :
             this(username, MakeSecureString(password), endPoint)
@@ -92,12 +94,6 @@ namespace Wdsf.Api.Client
         }
 
 
-        /// <summary>
-        /// Gets a list of persons.
-        /// </summary>
-        /// <param name="filter">A dictionaly containing filter parameters. See API documentation for details.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>A list of persons</returns>
         public IList<Person> GetPersons(IDictionary<string, string> filter)
         {
             if (filter == null)
@@ -107,25 +103,12 @@ namespace Wdsf.Api.Client
 
             return GetResourceList<ListOfPerson, Person>(string.Format("person?{0}", query));
         }
-
-        /// <summary>
-        /// Gets a person
-        /// </summary>
-        /// <param name="min">The member ID number</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The person model.</returns>
         public PersonDetail GetPerson(int min)
         {
             return GetResource<PersonDetail>(string.Format("person/{0}", min));
         }
 
 
-        /// <summary>
-        /// Gets a list of competitions.
-        /// </summary>
-        /// <param name="filter">A dictionaly containing filter parameters. See API documentation for details.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>A list of competitions.</returns>
         public IList<Competition> GetCompetitions(IDictionary<string, string> filter)
         {
             if (filter == null)
@@ -135,23 +118,10 @@ namespace Wdsf.Api.Client
 
             return GetResourceList<ListOfCompetition, Competition>(string.Format("competition?{0}", query));
         }
-
-        /// <summary>
-        /// Gets a competition
-        /// </summary>
-        /// <param name="id">The competition ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The competition</returns>
         public CompetitionDetail GetCompetition(int id)
         {
             return GetResource<CompetitionDetail>(string.Format("competition/{0}", id));
         }
-
-        /// <summary>
-        /// Updates a competition
-        /// </summary>
-        /// <param name="competition">The competition model</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
         public bool UpdateCompetition(CompetitionDetail competition)
         {
             if (competition == null)
@@ -162,36 +132,16 @@ namespace Wdsf.Api.Client
 
             return UpdateResource<CompetitionDetail>(competition, string.Format("competition/{0}", competition.Id));
         }
-
-
-        /// <summary>
-        /// Gets a participant (couple)
-        /// </summary>
-        /// <param name="id">The participant ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The participant (couple)</returns>
         public ParticipantCoupleDetail GetCoupleParticipant(int id)
         {
             return GetResource<ParticipantCoupleDetail>(string.Format("participant/{0}", id));
         }
-
-        /// <summary>
-        /// Get all participiants (couples) for specified competition
-        /// </summary>
-        /// <param name="competitionId">The competition ID</param>
-        /// <returns>The list of participants (couples)</returns>
         public IList<ParticipantCouple> GetCoupleParticipants(int competitionId)
         {
             return GetResourceList<ListOfCoupleParticpant, ParticipantCouple>(
                 string.Format("participant?competitionId={0}", competitionId)
             );
         }
-
-        /// <summary>
-        /// Updates a participant (couple).
-        /// </summary>
-        /// <param name="participant">The participant model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
         public bool UpdateCoupleParticipant(ParticipantCoupleDetail participant)
         {
             if (participant == null)
@@ -201,13 +151,6 @@ namespace Wdsf.Api.Client
 
             return UpdateResource<ParticipantCoupleDetail>(participant, string.Format("participant/{0}", participant.Id));
         }
-
-        /// <summary>
-        /// Saves a new participant (couple).
-        /// </summary>
-        /// <param name="participant">The participant model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The Uri to the newly created resource</returns>
         public Uri SaveCoupleParticipant(ParticipantCoupleDetail participant)
         {
             if (participant == null)
@@ -217,36 +160,16 @@ namespace Wdsf.Api.Client
 
             return SaveResource<ParticipantCoupleDetail>(participant, "participant");
         }
-
-        /// <summary>
-        /// Delete a participant (couple)
-        /// </summary>
-        /// <param name="id">The participant ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>true on success.</returns>
         public bool DeleteCoupleParticipant(int id)
         {
             return DeleteResource(string.Format("participant/{0}", id));
         }
 
 
-        /// <summary>
-        /// Gets a participant (team)
-        /// </summary>
-        /// <param name="id">The participant ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The participant (team)</returns>
         public ParticipantTeamDetail GetTeamParticipant(int id)
         {
             return GetResource<ParticipantTeamDetail>(string.Format("participant/{0}", id));
         }
-
-        /// <summary>
-        /// Updates a participant (team).
-        /// </summary>
-        /// <param name="participant">The participant model.</param>
-        /// <exception cref="UnknownMediaTypeException">The received resource type was not recognized.</exception>
-        /// <exception cref="UnexpectedMediaTypeException">The received resource type was not expected.</exception>
         public bool UpdateTeamParticipant(ParticipantTeamDetail participant)
         {
             if (participant == null)
@@ -256,13 +179,6 @@ namespace Wdsf.Api.Client
 
             return UpdateResource<ParticipantTeamDetail>(participant, string.Format("participant/{0}", participant.Id));
         }
-
-        /// <summary>
-        /// Saves a new participant (team).
-        /// </summary>
-        /// <param name="participant">The participant model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The Uri to the newly created resource</returns>
         public Uri SaveTeamParticipant(ParticipantTeamDetail participant)
         {
             if (participant == null)
@@ -272,47 +188,22 @@ namespace Wdsf.Api.Client
 
             return SaveResource<ParticipantTeamDetail>(participant, "participant");
         }
-
-        /// <summary>
-        /// Delete a participant (team)
-        /// </summary>
-        /// <param name="id">The participant ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>true on success.</returns>
         public bool DeleteTeamParticipant(int id)
         {
             return DeleteResource(string.Format("participant/{0}", id));
         }
 
 
-        /// <summary>
-        /// Gets an official
-        /// </summary>
-        /// <param name="id">The official ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The official</returns>
         public OfficialDetail GetOfficial(int id)
         {
             return GetResource<OfficialDetail>(string.Format("official/{0}", id));
         }
-
-        /// <summary>
-        /// Gets officials of specified competition
-        /// </summary>
-        /// <param name="competitionId">The competition ID</param>
-        /// <returns>The list of officials</returns>
         public IList<Official> GetOfficials(int competitionId)
         {
             return GetResourceList<ListOfOfficial, Official>(
                 string.Format("official?competitionId={0}", competitionId)
             );
         }
-
-        /// <summary>
-        /// Updates an official
-        /// </summary>
-        /// <param name="official">The official model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
         public bool UpdateOfficial(OfficialDetail official)
         {
             if (official == null)
@@ -320,13 +211,6 @@ namespace Wdsf.Api.Client
 
             return UpdateResource<OfficialDetail>(official, string.Format("official/{0}", official.Id));
         }
-
-        /// <summary>
-        /// Saves a new official.
-        /// </summary>
-        /// <param name="official">The official model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The Uri to the newly created official</returns>
         public Uri SaveOfficial(OfficialDetail official)
         {
             if (official == null)
@@ -334,35 +218,16 @@ namespace Wdsf.Api.Client
 
             return SaveResource<OfficialDetail>(official, "official");
         }
-
-        /// <summary>
-        /// Delete am official
-        /// </summary>
-        /// <param name="id">The official ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>true on success.</returns>
         public bool DeleteOfficial(int id)
         {
             return DeleteResource(string.Format("official/{0}", id));
         }
 
 
-        /// <summary>
-        /// Gets a list of all active couples. This list contains all neccesary information for off-line usage.
-        /// </summary>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>A list of couples</returns>
         public IList<CoupleExport> GetCouples()
         {
             return GetResourceList<ListOfCoupleExport, CoupleExport>("couple/export");
         }
-
-        /// <summary>
-        /// Gets a list of couples.
-        /// </summary>
-        /// <param name="filter">A dictionaly containing filter parameters. See API documentation for details.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>A list of couples</returns>
         public IList<Couple> GetCouples(IDictionary<string, string> filter)
         {
             if (filter == null)
@@ -372,13 +237,6 @@ namespace Wdsf.Api.Client
 
             return GetResourceList<ListOfCouple, Couple>(string.Format("couple?{0}", query));
         }
-
-        /// <summary>
-        /// Gets a couple
-        /// </summary>
-        /// <param name="id">The couple ID</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The couple</returns>
         public CoupleDetail GetCouple(string id)
         {
             if (string.IsNullOrEmpty(id))
@@ -386,12 +244,6 @@ namespace Wdsf.Api.Client
 
             return GetResource<CoupleDetail>(string.Format("couple/{0}", id));
         }
-
-        /// <summary>
-        /// Updates a couple
-        /// </summary>
-        /// <param name="couple">The couple model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
         public bool UpdateCouple(CoupleDetail couple)
         {
             if (couple == null)
@@ -399,13 +251,6 @@ namespace Wdsf.Api.Client
 
             return UpdateResource<CoupleDetail>(couple, string.Format("couple/{0}", couple.Id));
         }
-
-        /// <summary>
-        /// Saves a new couple.
-        /// </summary>
-        /// <param name="couple">The official model.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The Uri to the newly created couple</returns>
         public Uri SaveCouple(CoupleDetail couple)
         {
             if (couple == null)
@@ -415,16 +260,10 @@ namespace Wdsf.Api.Client
         }
 
 
-        /// <summary>
-        /// Gets a world ranking list.
-        /// </summary>
-        /// <param name="discipline">The discipline.</param>
-        /// <param name="ageGroup">The age group.</param>
-        /// <param name="division">The division.</param>
-		/// <param name="date">Date to get ranking for. Can be absent.</param>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <exception cref="ArgumentException">Any of the required arguments is missing.</exception>
-        /// <returns>The world ranking list</returns>
+        public IList<Ranking> GetWorldRanking(string discipline, string ageGroup, string division)
+        {
+            return GetWorldRanking(discipline, ageGroup, division, DateTime.MinValue);
+        }
 		public IList<Ranking> GetWorldRanking(string discipline, string ageGroup, string division, DateTime date)
         {
             if (string.IsNullOrEmpty(discipline))
@@ -442,26 +281,19 @@ namespace Wdsf.Api.Client
                 throw new ArgumentException("Division must be set.", "division");
             }
 
-            return GetResourceList<ListOfRanking, Ranking>(
-				string.Format("ranking?agegroup={0}&discipline={1}&division={2}&date={3:yyyy\\/MM\\/dd}", ageGroup, discipline, division)
-            );
+            string resourceUri = 
+                date != DateTime.MinValue ?
+                string.Format("ranking?agegroup={0}&discipline={1}&division={2}&date={3:yyyy/MM/dd}", ageGroup, discipline, division, date) :
+                string.Format("ranking?agegroup={0}&discipline={1}&division={2}", ageGroup, discipline, division);
+
+            return GetResourceList<ListOfRanking, Ranking>(resourceUri);
         }
 
-        /// <summary>
-        /// Gets all allowed age groups.
-        /// </summary>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The age groups</returns>
         public IList<AgeClass> GetAges()
         {
             return GetResourceList<ListOfAgeClass, AgeClass>("age");
         }
 
-        /// <summary>
-        /// Gets all allowed countries.
-        /// </summary>
-        /// <exception cref="ApiException">The request failed. See inner exception for details.</exception>
-        /// <returns>The countries</returns>
         public IList<Country> GetCountries()
         {
             return GetResourceList<ListOfCountry, Country>("country");

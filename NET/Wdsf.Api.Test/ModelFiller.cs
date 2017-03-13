cb7a48b3339f4a97471aa6de385124c2011548c5
jaykay-design <jay@jaykay-design.com>
2017-03-13 12:41:29 +0100

    implement OnScale3 model filler

--- NET/Wdsf.Api.Test/ModelFiller.cs
+++ NET/Wdsf.Api.Test/ModelFiller.cs
@@ -16,31 +16,77 @@ namespace Wdsf.Api.Test
                 return;
             }
 
-            var adjudicators = officials.Where(o=> o.official.Task == "Adjudicator");
-            var chairman = officials.FirstOrDefault(o=> o.official.Task == "Chairman");
+            var adjudicators = officials.Where(o => o.official.Task == "Adjudicator");
+            var chairman = officials.FirstOrDefault(o => o.official.Task == "Chairman");
             Random rand = new Random();
-            if (scoreType == "Scating")
+            for (int roundIndex = 1; roundIndex <= rounds; roundIndex++)
             {
-                for (int roundIndex = 1; roundIndex <= rounds; roundIndex++)
+                Round round = new Round() { Name = roundIndex == maxRounds ? "F" : roundIndex.ToString() };
+                couple.Rounds.Add(round);
+                for (int danceIndex = 0; danceIndex < 5; danceIndex++)
                 {
-                    Round round = new Round() { Name = roundIndex == maxRounds ? "F" : roundIndex.ToString() };
-                    couple.Rounds.Add(round);
-                    for (int danceIndex = 0; danceIndex < 5; danceIndex++)
+                    Dance dance = new Dance() { Name = danceNames[danceIndex] };
+                    round.Dances.Add(dance);
+                    switch (scoreType)
                     {
-                        Dance dance = new Dance(){ Name = danceNames[danceIndex]};
-                        round.Dances.Add(dance);
-                        switch (scoreType)
-                        {
-                            case "Scating":
+                        case "Scating":
+                            {
+                                if (roundIndex == maxRounds)
                                 {
-                                    if (roundIndex == maxRounds)
+                                    foreach (var adj in adjudicators)
+                                    {
+                                        dance.Scores.Add(new FinalScore()
+                                        {
+                                            OfficialId = adj.official.Id,
+                                            Rank = rand.Next(1, 6)
+                                        });
+                                    }
+                                }
+                                else
+                                {
+                                    foreach (var adj in adjudicators)
+                                    {
+                                        if (rand.Next(0, 100) > 50)
+                                        {
+                                            dance.Scores.Add(new MarkScore()
+                                            {
+                                                OfficialId = adj.official.Id
+                                            });
+                                        }
+                                    }
+                                }
+
+                                break;
+                            }
+                        case "OnScale 1":
+                            {
+
+                                break;
+                            }
+                        case "OnScale 2":
+                            {
+
+                                break;
+                            }
+                        case "OnScale 3":
+                            {
+                                if (roundIndex == maxRounds)
+                                {
+                                    bool isGroupDance = rand.Next(0, 10) >= 5;
+                                    dance.IsGroupDance = isGroupDance;
+
+                                    if (isGroupDance)
                                     {
                                         foreach (var adj in adjudicators)
                                         {
-                                            dance.Scores.Add(new FinalScore()
+                                            int scoreCount = 0;
+                                            dance.Scores.Add(new OnScale3Score()
                                             {
                                                 OfficialId = adj.official.Id,
-                                                Rank = rand.Next(1,6)
+                                                CP = rand.Next(0, 10) >= 5 && scoreCount++ == 0 ? rand.Next(5, 10) : 0,
+                                                MM = rand.Next(0, 10) >= 5 && scoreCount++ == 0 ? rand.Next(5, 10) : 0,
+                                                PS = rand.Next(0, 10) >= 5 && scoreCount++ == 0 ? rand.Next(5, 10) : 0,
+                                                TQ = scoreCount == 0 ? rand.Next(5, 10) : 0
                                             });
                                         }
                                     }
@@ -48,33 +94,37 @@ namespace Wdsf.Api.Test
                                     {
                                         foreach (var adj in adjudicators)
                                         {
-                                            if (rand.Next(0, 100) > 50)
+                                            int scoreCount = 0;
+                                            dance.Scores.Add(new OnScale3Score()
                                             {
-                                                dance.Scores.Add(new MarkScore()
-                                                {
-                                                    OfficialId = adj.official.Id
-                                                });
-                                            }
+                                                OfficialId = adj.official.Id,
+                                                CP = rand.Next(0, 10) >= 5 && scoreCount++ < 2 ? rand.Next(5, 10) : 0,
+                                                MM = rand.Next(0, 10) >= 5 && scoreCount++ < 2 ? rand.Next(5, 10) : 0,
+                                                PS = scoreCount++ < 2 ? rand.Next(5, 10) : 0,
+                                                TQ = scoreCount < 2 ? rand.Next(5, 10) : 0
+                                            });
                                         }
                                     }
-
-                                    break;
                                 }
-                            case "OnScale 1":
+                                else
                                 {
-
-                                    break;
+                                    foreach (var adj in adjudicators)
+                                    {
+                                        if (rand.Next(0, 100) > 50)
+                                        {
+                                            dance.Scores.Add(new MarkScore()
+                                            {
+                                                OfficialId = adj.official.Id
+                                            });
+                                        }
+                                    }
                                 }
-                            case "OnScale 2":
-                                {
 
-                                    break;
-                                }
-                        }
+                                break;
+                            }
                     }
                 }
             }
-
         }
     }
 }
